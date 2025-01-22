1b776e1413a7e2e529485c36f79741e0f1346b97
Gabrielle Santiago Silva <gabriellesantisilva@gmail.com>
2025-01-21 22:01:41 -0300

    Organização das pastas

---
+++ GameCatalog/src/test/java/com/gabrielle_santiago/GameCatalog/GameCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielle_santiago.GameCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
